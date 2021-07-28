995afd66011fecaf8900c3593fa5c08678ba8bf5
SimJaeuk <jaehook293@gmail.com>
2021-07-28 15:49:06 +0900

    spring basic

--- Basic/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ Basic/hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,9 +24,9 @@ class MemberServiceIntegrationTest {
 
     @Test
     void 회원가입() {
-        //given
+        //given`
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring100");
 
         //when
         Long saveId = memberService.join(member);
